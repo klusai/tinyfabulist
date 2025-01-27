f8b075d62b97da1f0ca660edfa30efbf74f98e1c
andreeatomescu16 <andreeatomescu040@gmail.com>
2025-01-27 09:39:55 +0200

    Deleted files

--- phi-4/hf_models2.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import HfApi
-
-api = HfApi()
-model_id = "distilbert-base-uncased"  # Known to have Apache-2.0 license
-info = api.model_info(model_id)
-
-print("Top-level license:", info.license)
-print("cardData license:", info.cardData.get("license") if info.cardData else None)
