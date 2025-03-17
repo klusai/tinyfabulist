97aaba7246506d26f945b7d7b62ff0aaa8d02bab
ap <andrei.piscoran@klusai.com>
2025-03-17 09:30:07 +0200

    Entrypoint fix

--- tinyfabulist.py
+++ tinyfabulist.py
@@ -2,7 +2,7 @@ import argparse
 from stats import add_stats_subparser
 from generate import add_generate_subparser
 from evaluate.en import add_evaluate_subparser
-from translate.deepl import add_translate_subparser
+from translate.deepl import deepl_subparser
 
 def main():
     parser = argparse.ArgumentParser(description='TinyFabulist - Fable generator and evaluator')
@@ -10,7 +10,7 @@ def main():
     add_generate_subparser(subparsers)
     add_evaluate_subparser(subparsers)
     add_stats_subparser(subparsers)
-    add_translate_subparser(subparsers)
+    deepl_subparser(subparsers)
     args = parser.parse_args()
 
     if hasattr(args, 'func'):
